f67cc5c789febb1b444f11a531fb3a3ea29a7462
François MATHIEU <francois.mathieu@livexp.fr>
2018-02-28 10:27:42 +0100

    Add datepicker

---
+++ web/Resources/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        todayBtn: "linked",
+        language: "fr",
+        clearBtn: true,
+        autoclose: true,
+        todayHighlight: true
+    });
+
+});
